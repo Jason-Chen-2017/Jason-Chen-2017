6cbae01a068a98706675cb03fe2643489fa2c734
15868187925 <15868187925@163.com>
2018-02-14 03:54:37 +0800

    云编程 IDE Dev Tools

--- public/js/header.js
+++ public/js/header.js
@@ -1,35 +1,17 @@
 var Header = React.createClass({
 
     render: function () {
-        var h1Style = {
-            marginLeft: '20',
-            width:'100%'
-        };
-        var h2Style = {
-            marginLeft: '40',
-            width:'100%'
-        };
-
-        var headerStyle={
-            width:'100%'
-        }
-
         return (
-            <header style={headerStyle}>
-                <div class="">
-                    <h1 style={h1Style}>陈光剑的个人网站</h1>
-                    <h2 style={h2Style}>Jason Chen's Personal Website</h2>
-
-                    <a href="https://github.com/Jason-Chen-2017/Jason-Chen-2017" class="button">
-                        <small>View project on</small>
-                        GitHub</a>
-                </div>
+            <header>
+                <h1>东海陈光剑的个人网站</h1>
+                <h2><a href={'https://jason-chen-2017.github.io/Jason-Chen-2017/'}> Jason Chen's Personal Website </a>
+                </h2>
             </header>
         );
     }
 });
 
-ReactDOM.render(<Header />, document.getElementById('header'));
+ReactDOM.render(<Header/>, document.getElementById('header'));
 
 
 
--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -21,22 +21,31 @@ var SideBar = React.createClass({
         };
 
         var btnStyle = {
-            marginTop: '1em',
-            marginBottom: '1em'
+            margin: '1em 0em',
+            padding: '1em',
+            border: '1px solid',
+            borderRadius: '1em',
+            borderColor: 'dodgerblue',
+            color: 'cornflowerblue',
+            textAlign: 'center',
         };
 
         return (
             <div style={helloWorldStyle}>
+                <hr></hr>
                 <aside id="sidebar">
-                    <button onClick={this.goHome} style={btnStyle}>回到首页</button>
-                    <p class="repo-owner"> Author: <a href="https://github.com/Jason-Chen-2017" target="_blank">Jason Chen,2017</a>.</p>
+                    <div onClick={this.goHome} style={btnStyle}>回到首页</div>
+
                     <p>作者:
                         陈光剑，江苏东海人, 号行走江湖一剑客，字之剑。程序员，诗人, 作家。邮箱: universsky@163.com, 微信:universskyoh</p>
 
-                    <button onClick={this.goFreeBook} style={btnStyle}>光剑免费图书馆</button>
+                    <div onClick={this.goFreeBook} style={btnStyle}>光剑免费图书馆</div>
 
                     <p data-myattribute="somevalue">道之所在，虽千万人吾往矣；义之所当，千金散尽不后悔；情之所钟，世俗礼法如粪土；兴之所至，与君痛饮三百杯.(Jason
                         Chen,2017)</p>
+
+                    <p class="repo-owner"> Author: <a href="https://github.com/Jason-Chen-2017" target="_blank">Jason
+                        Chen,2017</a>.</p>
 
                 </aside>
             </div>
@@ -46,4 +55,4 @@ var SideBar = React.createClass({
 
 
 //执行渲染引擎
-ReactDOM.render(<SideBar />, document.getElementById('sidebar'));
+ReactDOM.render(<SideBar/>, document.getElementById('sidebar'));

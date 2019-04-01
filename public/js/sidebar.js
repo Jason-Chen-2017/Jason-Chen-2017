bde05d3e5e585dc3bea257eca226972ea038eab4
jack <guangjian.cgj@alibaba-inc.com>
2019-04-01 23:50:00 +0800

    人间味:60首.html 夜精灵:53首.html 拾叶:24首.html 春心沁透:99首.html 自渡:125首.html 青玉案:27首.html

--- public/js/sidebar.js
+++ public/js/sidebar.js
@@ -36,11 +36,13 @@ var SideBar = React.createClass({
                 <aside id="sidebar">
                     <div onClick={this.goHome} style={btnStyle}>回到首页</div>
 
-                    <div>作者:
-                        陈光剑，江苏东海人, 号行走江湖一剑客，字之剑。程序员，诗人, 作家。
+                    <div>
+                        关于作者: 陈光剑,江苏东海人。程序员,诗人,作家。
                     </div>
                     <div>邮箱: universsky@163.com</div>
-                    <div>微信:universskyoh</div>
+                    <div>微信: universskyoh</div>
+                    <div><a href={"https://www.jianshu.com/u/c55c7a9c8de6"} target={"_blank"}>简书</a></div>
+                    <div><a href={"https://weibo.com/oranread"} target={"_blank"}>微博</a></div>
 
                     <hr></hr>
                     <div data-myattribute="somevalue">
@@ -48,7 +50,6 @@ var SideBar = React.createClass({
                         <div>义之所当，千金散尽不后悔；</div>
                         <div>情之所钟，世俗礼法如粪土；</div>
                         <div>兴之所至，与君痛饮三百杯.</div>
-                        <div>(Jason Chen,2017)</div>
                     </div>
 
                     <div onClick={this.goFreeBook} style={btnStyle}>光剑免费图书馆</div>
